53e55db4f02f10a7911f7c334a52fd79c20be616
Sebastian Place <sebplace94@gmail.com>
2016-01-21 20:14:39 +0000

    edit goal

--- app/routes/goals/edit.js
+++ app/routes/goals/edit.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  //action from rarwe!
+  actions: {
+    save() {
+      const controller = this.get('controller'),
+            goal = controller.get('model');
 
+      return goal.save();
+    },
+  }
 
 });
