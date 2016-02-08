ed36134857ff478f81de5270820c90ec3de6022b
Sebastian Place <sebplace94@gmail.com>
2016-02-08 17:31:43 +0000

    Update dependencies and ember-cli, add validations(not working)

--- app/models/goal.js
+++ app/models/goal.js
@@ -1,6 +1,21 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
-export default DS.Model.extend({
+import { validator, buildValidations } from 'ember-cp-validations';
+//TODO mkae validatons work, perhaps add code to templates
+var Validations = buildValidations({
+  title: {
+    debounce: 500,
+    validators: [
+      validator('presence', true),
+      validator('length', {
+        max: 15
+      })
+    ]
+  },
+});
+
+export default DS.Model.extend(Validations, {
   title: DS.attr('string'),
   description: DS.attr('string'),
   dateCreated: DS.attr('date'),
