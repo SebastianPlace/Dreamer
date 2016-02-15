ef4ba4bef9e660aa4de188312dbfa745c778cb0d
Sebastian Place <sebplace94@gmail.com>
2016-02-15 10:22:37 +0000

    Edit habit bug fix

--- app/components/habit-card.js
+++ app/components/habit-card.js
@@ -1,22 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['habit-card'],
+  classNames: ['col-md-4','habit-col'],
   isEditing: false,
-
+  oldTitle:'',
   actions:{
     edit(){
       this.set('isEditing', true);
+      this.set('oldTitle',this.get('habit.title'));
     },
     save(){
       this.set('isEditing', false);
+      this.set('oldTitle', '');
       return this.habit.save();
     },
     delete(){
       this.habit.destroyRecord();
     },
     cancel(){
+      this.set('habit.title', this.get('oldTitle'));
       this.set('isEditing', false);
+    },
+    logNo(){
+      //this.habit.dateLog;
+      console.log("NAAGAHAHHAG!!");
+    },
+    logYes(){
+      console.log("YESSSSS");
     }
+
   }
 });
