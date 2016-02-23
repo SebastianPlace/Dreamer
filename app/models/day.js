14059569735e8c5fba0ba0ee4993d07a796a674f
Sebastian Place <sebplace94@gmail.com>
2016-02-23 09:12:33 +0000

    tooltips

--- app/models/day.js
+++ app/models/day.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  habit:DS.belongsTo('habit'),
+  habit: DS.belongsTo('habit'),
   date: DS.attr(),
   isCompleted: DS.attr('boolean')
 });
--- app/routes/goals.js
+++ app/routes/goals.js
@@ -1,8 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  //TODO find only the goals that belong to the current user
+  //TODO: http://stackoverflow.com/questions/33717667/manage-ember-multiple-rsvp-promises-on-the-same-route
   model() {
+    // const currentUserId = this.get('session.currentUser.id');
+    // this.store.query('user', { filter: { uid:currentUserId  }}).then((user)=>{
+    //   return this.store.query('goal', {orderBy: 'user', equalTo: user.objectAt(0).id });
+    // });
     return this.store.findAll('goal');
+
+
+
   },
 });
