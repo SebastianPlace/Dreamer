ea021d7a00e534ddb3ebc9f0f2fa93efedbc8df7
Sebastian Place <sebplace94@gmail.com>
2016-03-02 22:44:04 +0000

    ember cli & dependencies update

--- app/components/habit-day.js
+++ app/components/habit-day.js
@@ -3,8 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames:['day-circle'],
   classNameBindings: ['day.isCompleted:complete:incomplete'],
+  isToday: Ember.computed('this.day',{
 
+  }),
   click(){
     console.log(this.day.get('isCompleted'));
   }
+
 });
