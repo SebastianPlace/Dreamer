c9ed240ed2afcd2d7cf22f7ac679695fee13ce6d
Sebastian Place <sebplace94@gmail.com>
2016-04-02 01:09:31 +0100

    Improve day logging. Now much less error prone

--- app/components/habit-card.js
+++ app/components/habit-card.js
@@ -11,38 +11,13 @@ export default Ember.Component.extend({
   startTime: null,
   endDate: null,
   endTime: null,
+  isAddingDays: false,
 
-  //TODO use this function to style habit when it is completed for the day
-  //TODO change to if today on datepicker, is complete.
-  // isTodayDone: Ember.computed.bool('habit.days', function(){
-  //   const days = this.get('habit.days');
-  //   const currentDay= new Date().toDateString();
-  //   const today = days.filterBy('date',currentDay);
-  //   if(today.get('isCompleted')){
-  //     return true;
-  //   }
-  // }),
-
-
-  //This save is scheduled to run within a separate run loop with a 1 ms wait.
-  //See: http://emberjs.com/api/classes/Ember.run.html#method_next
-  saveHabit: function(){
-    Ember.run.next(()=>{
-      this.habit.save().catch((err)=>{
-        console.error(err);
-      });
-    });
-  },
-  //This is not the most effective way to do this.
-  //When multiple dates are clicked quickly, only some are saved.
-  //TODO wait until user has entered all days before saving!
   datesDidChange : function() {
-    //wait for habit attrs to change before save is called
-    //throttle prevents habit from being saved before the model is habit attributes have changed
     if(this.get('habit.hasDirtyAttributes')){
-      Ember.run.throttle(this, this.saveHabit, 250);
+      this.set('isAddingDays', true);
     }
-  }.observes('habit.hasDirtyAttributes'),
+  }.observes('habit.hasDirtyAttributes').on('init'),
 
   concatDateTime(date, time){
     const hours = parseInt(time.substring(0, 2));
@@ -68,7 +43,9 @@ export default Ember.Component.extend({
     },
 
     edit(){
+      this.habit.rollbackAttributes();
       this.set('isEditing', true);
+      this.set('isAddingDays', false);
     },
 
     save(){
@@ -77,6 +54,7 @@ export default Ember.Component.extend({
       .then(() => {
         flashMessages.success('Changes saved successfully!');
         this.set('isEditing', false);
+        this.set('isAddingDays', false);
       })
       .catch((err) => {
         flashMessages.danger('Whoops. Your changes could not be saved.');
@@ -97,6 +75,7 @@ export default Ember.Component.extend({
     cancel(){
       this.habit.rollbackAttributes();
       this.set('isEditing', false);
+      this.set('isAddingDays', false);
     },
   }
 });
